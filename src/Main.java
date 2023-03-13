ab7974ddc52ee2fc960b3abc0ba4565826709d81
Aigerim <a.zhamalbekova@gmail.com>
2023-03-13 15:35:37 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        one(10);
+    }
+    public static void one (int a) {
+        for (int i = 0; i < a ; i++) {
+            System.out.print(" * " );
+        }
+    }
+}
